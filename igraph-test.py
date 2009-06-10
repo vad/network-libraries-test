4d6e77f955b803d08516ec8beee40b86006733d7
Davide Setti <davide.setti@gmail.com>
2009-06-10 15:54:39 +0200

    igraph: changes in average distance

--- igraph-test.py
+++ igraph-test.py
@@ -5,7 +5,6 @@ from time import ctime
 from getopt import *
 import sys
 
-
 ## GLOBAL VARIABLES
 
 ## FUNCTIONS
@@ -30,10 +29,12 @@ mean:   mean degree for this type
 def printAverageDistance(g):
     print 'DISTANCES', ctime()
     dSum = 0
+    print 'Len:', len(g.vs)
     for i in range(len(g.vs)):
-        print i
+        if not i % 100:
+            print 'Step:', i
+
         dSum += sum(g.shortest_paths_dijkstra(i, weights='weight')[0])
-    #distances = giant.shortest_paths_dijkstra()
 
     print 'AVERAGE', ctime()
     avg_dist = 1.*dSum / (len(g.vs) * (len(g.vs)-1))
@@ -49,14 +50,14 @@ def usage(error = 0):
 
 if __name__ == '__main__':
     try:                                
-        opts, args = getopt(sys.argv[1:], "hdeir", ["help", "details", "degree", "distance", 'density']) 
+        opts, args = getopt(sys.argv[1:], "hdeirt", ["help", "details", "degree", "distance", 'density', 'transitivity']) 
     except GetoptError:
         usage(2)
 
     if len(args) != 1:
         usage(1)
 
-    _details = _degree = _distance = _density = False
+    _details = _degree = _distance = _density = _transitivity = False
     for opt, arg in opts:
         if opt in ('-d', '--details'):
             _details = True
@@ -66,6 +67,8 @@ if __name__ == '__main__':
             _distance = True
         if opt in ('-r', '--density'):
             _density = True
+        if opt in ('-t', '--transitivity'):
+            _transitivity = True
 
     fn = args[0]
     g = igraph.load(fn)
@@ -91,13 +94,15 @@ if __name__ == '__main__':
         print "Variance IN Degree: %f" % degreeVariance(g, igraph.IN, mid)
         print "Variance OUT Degree: %f" % degreeVariance(g, igraph.OUT, mod)
 
+    if _transitivity:
+        print " * transitivity: %f" % (nx.transitivity(g), )
+    
     if _distance:
-        cl = g.clusters()
+        giant = g.clusters().giant()
 
-        giant = cl.giant()
         print "Length max cluster: %d" % (len(giant.vs), )
 
-        #printAverageDistance(giant)
+        printAverageDistance(giant)
 
         #print "Average distance 2: %f" % giant.average_path_length(True, False)
 
